f3c23abaca9d7cabbc204621399246d37287a1e9
anbo@easj.dk <gitterly12>
2014-09-23 21:00:31 +0200

    HttpServer constructor + very basic Run() method

--- httpserver/HttpServer.cs
+++ httpserver/HttpServer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +10,20 @@ namespace httpserver
     public class HttpServer
     {
         public static readonly int DefaultPort = 8888;
+        public int Port { get; private set; }
+
+        public HttpServer(int port)
+        {
+            Port = port;
+        }
+
+        public void Run()
+        {
+            // Start server
+            while (true)
+            {
+                TcpClient clientConnection = 
+            }
+        }
     }
 }
